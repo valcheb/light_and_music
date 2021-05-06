25bac589701f1bcdc37d9520f2f7a3998739b949
valcheb <cheb.valentin@gmail.com>
2021-05-06 10:37:17 +0300

    fix(microphone): add forgotten lines

--- src/microphone.h
+++ src/microphone.h
@@ -2,6 +2,7 @@
 #define MICROPHONE_H_
 
 #include <stdbool.h>
+#include <stdint.h>
 
 #define MIC_GAIN     75
 #define MIC_PDM_SIZE 64 //1ms for 16kHz input
@@ -10,6 +11,7 @@
 void mic_init(void);
 void mic_enable(void);
 bool mic_rx_ready(void);
+void mic_rx_ready_clear(void);
 void mic_pdm_pcm_convert(uint16_t *pcm_buffer);
 
 #endif /*MICROPHONE_H_*/
