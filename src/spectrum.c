dca0fb156225076138428b135204464cb20329e4
valcheb <cheb.valentin@gmail.com>
2021-05-11 19:20:27 +0300

    feat(spectrum): calculate coefficients in frequency ranges

--- src/spectrum.c
+++ src/spectrum.c
@@ -1,7 +1,6 @@
 #include "spectrum.h"
 #include "lm_config.h"
 
-#include "arm_math.h"
 #include "arm_const_structs.h"
 
 static float32_t fft_input_buffer[2 * LM_FFT_BUFFER_SIZE];
@@ -30,20 +29,56 @@ void spectrum_init()
     }
 }
 
-void spectrum_analysis(uint16_t *sound)
+void spectrum_analysis(uint16_t *sound, float32_t *coeff)
 {
+    const static uint8_t ifftFlag = 0;
+    const static uint8_t doBitReverse = 1;
+    const static int block_size = (LM_FFT_BUFFER_SIZE / 2) / LM_CHANNEL_NUMBER;
+    const static float32_t noise_gate = 0.02;
+
+    float32_t window_energy = 0.0;
+
     for (int i = 0; i < LM_FFT_BUFFER_SIZE; i++)
     {
-        fft_input_buffer[2 * i] = (float32_t)( (float32_t)sound[i]);
+        fft_input_buffer[2 * i] = (float32_t)sound[i] / (0xffff * 0.5) - 1; //place signal between -1 and 1
         fft_input_buffer[2 * i + 1] = 0;
     }
 
-    uint8_t ifftFlag = 0;
-    uint8_t doBitReverse = 1;
     arm_cfft_f32(s, fft_input_buffer, ifftFlag, doBitReverse);
     arm_cmplx_mag_f32(fft_input_buffer, mag_output_buffer, LM_FFT_BUFFER_SIZE);
 
-    float32_t maxValue;
-    uint32_t maxIndex;
-    arm_max_f32(mag_output_buffer + 1, LM_FFT_BUFFER_SIZE - 1, &maxValue, &maxIndex); //+1 - avoid zero harmonic
+    for (int i = 0; i < LM_FFT_BUFFER_SIZE; i++)
+    {
+        window_energy += mag_output_buffer[i] * mag_output_buffer[i];
+    }
+    window_energy /= LM_FFT_BUFFER_SIZE;
+
+    for (int i = 0; i < LM_CHANNEL_NUMBER; i++)
+    {
+        /*
+        float32_t maxValue = 0.0;
+        uint32_t maxIndex = 0;
+        arm_max_f32(mag_output_buffer + 1 + block_size * i, block_size, &maxValue, &maxIndex); //+1 - avoid zero harmonic
+        coeff[i] = maxValue / window_energy - noise_gate;
+        */
+
+        float32_t block_energy = 0.0;
+        for (int j = block_size * i; j < block_size * (i + 1); j++)
+        {
+            if (j == 0) //avoid zero harmonic
+            {
+                continue;
+            }
+            block_energy += mag_output_buffer[j] * mag_output_buffer[j];
+        }
+        block_energy /= LM_FFT_BUFFER_SIZE;
+        coeff[i] = block_energy / window_energy - noise_gate;
+
+        if (coeff[i] < 0)
+        {
+            coeff[i] = 0;
+        }
+    }
+
+    //pwm_array[i] = (uint16_t)( (max_array[i] * 1.5) * 0xffff);
 }
--- src/spectrum.h
+++ src/spectrum.h
@@ -2,8 +2,9 @@
 #define SPECTRUM_H_
 
 #include <stdint.h>
+#include "arm_math.h"
 
 void spectrum_init(void);
-void spectrum_analysis(uint16_t *sound);
+void spectrum_analysis(uint16_t *sound, float32_t *coeff);
 
 #endif /*SPECTRUM_H_*/
