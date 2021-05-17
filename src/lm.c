f43e0dc121d23399b5391a8c2ddfe82386c63698
valcheb <cheb.valentin@gmail.com>
2021-05-17 20:30:55 +0300

    refactor(lm): replace local counter by global sound_buffer_wpos

--- src/lm.c
+++ src/lm.c
@@ -10,6 +10,7 @@
 #include "spectrum.h"
 
 static uint16_t sound_buffer[LM_SOUND_BUFFER_SIZE];
+static int sound_buffer_wpos = 0;
 static uint16_t pcm_buffer[MIC_PCM_SIZE];
 static float32_t coeff_buffer[LM_CHANNEL_NUMBER];
 
@@ -47,18 +48,17 @@ void lm_enable()
 
 void lm_process()
 {
-    static int counter = 0;
     if (mic_rx_ready())
     {
         mic_rx_ready_clear();
         mic_pdm_pcm_convert(pcm_buffer);
-        memcpy(sound_buffer + counter * MIC_PCM_SIZE, pcm_buffer, MIC_PCM_SIZE * sizeof(uint16_t));
-        counter++;
+        memcpy(sound_buffer + sound_buffer_wpos * MIC_PCM_SIZE, pcm_buffer, MIC_PCM_SIZE * sizeof(uint16_t));
+        sound_buffer_wpos++;
     }
 
-    if ( (counter * MIC_PCM_SIZE) == LM_SOUND_BUFFER_SIZE)
+    if ( (sound_buffer_wpos * MIC_PCM_SIZE) == LM_SOUND_BUFFER_SIZE)
     {
-        counter = 0;
+        sound_buffer_wpos = 0;
 
         if (LM_USE_PLAYBACK)
         {
