282d21de765ec5d2288f56880a5262e650ed620e
valcheb <cheb.valentin@gmail.com>
2021-05-18 10:22:34 +0300

    refactor(microphone): add documentation

--- src/microphone.h
+++ src/microphone.h
@@ -8,10 +8,37 @@
 #define MIC_PDM_SIZE 64 //1ms for 16kHz input
 #define MIC_PCM_SIZE 16 //pdm_filter_init.Fs == 16kHz / 1000
 
+/**
+ * @brief Initialize functionality of MP45DT02 microphone
+ *
+ */
 void mic_init(void);
+
+/**
+ * @brief Enable functionality of MP45DT02 microphone
+ *
+ */
 void mic_enable(void);
+
+/**
+ * @brief Get readiness of microphone receiver buffer for processing
+ *
+ * @return true  Buffer is ready
+ * @return false Buffer is not ready
+ */
 bool mic_rx_ready(void);
+
+/**
+ * @brief Clear flag of readiness of microphone receiver buffer
+ *
+ */
 void mic_rx_ready_clear(void);
+
+/**
+ * @brief Convert internal pdm buffer to external pcm buffer
+ *
+ * @param pcm_buffer Pointer to result of conversion
+ */
 void mic_pdm_pcm_convert(uint16_t *pcm_buffer);
 
 #endif /*MICROPHONE_H_*/
