f5324f5d7ebeabdcfcbf1bf769fed1218db5ad3b
valcheb <cheb.valentin@gmail.com>
2021-05-18 09:45:28 +0300

    refactor(button): add documentation

--- src/button.h
+++ src/button.h
@@ -3,9 +3,31 @@
 
 #include <stdbool.h>
 
+/**
+ * @brief Initialize functionality of button
+ *
+ */
 void btn_init(void);
+
+/**
+ * @brief Set state of button
+ *
+ * @param state New state
+ */
 void btn_set_state(bool state);
+
+/**
+ * @brief Get state of button
+ *
+ * @return true  Button is pressed
+ * @return false Button is not pressed
+ */
 bool btn_get_state(void);
+
+/**
+ * @brief Software removing of button jitter
+ *
+ */
 void btn_delete_jitter(void);
 
 #endif /*BUTTON_H_*/
