627d9bebaa3d784528b6d4c15dede6fdf23b304f
valcheb <cheb.valentin@gmail.com>
2021-05-18 10:37:00 +0300

    refactor(pwm): add documentation

--- src/pwm.h
+++ src/pwm.h
@@ -17,10 +17,39 @@ typedef enum
     PWM_CHANNEL_ENUM_SIZE
 } pwm_channel_e;
 
+/**
+ * @brief Initialize functionality of pwm led indication
+ *
+ * @param channel_number Number of leds (pwm channels)
+ */
 void pwm_init(int channel_number);
+
+/**
+ * @brief Initialize functionality of error led indication
+ *
+ */
 void pwm_error_init(void);
+
+/**
+ * @brief Display led error indication
+ *
+ * Call in infinite loop
+ *
+ */
 void pwm_error_iter(void);
+
+/**
+ * @brief Set duty cycle of pwm channel
+ *
+ * @param channel Pwm channel number
+ * @param duty    Value of brightness
+ */
 void pwm_set_duty_cycle(pwm_channel_e channel, uint16_t duty);
+
+/**
+ * @brief Demonstration of leds serviceability
+ *
+ */
 void simple_leds_check(void);
 
 #endif /*PWM_H_*/
