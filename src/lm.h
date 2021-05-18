591012bbdfd9be8ea64e1acf096f7419198f026c
valcheb <cheb.valentin@gmail.com>
2021-05-18 10:08:30 +0300

    refactor(lm): add documentation

--- src/lm.h
+++ src/lm.h
@@ -8,8 +8,24 @@ typedef enum
     LM_LED_FUNC_ENUM_SIZE
 } lm_led_func_e;
 
+/**
+ * @brief Initialize functionality of light music
+ *
+ */
 void lm_init(void);
+
+/**
+ * @brief Enable functionality of light music
+ *
+ */
 void lm_enable(void);
+
+/**
+ * @brief Execute main processing of light music
+ *
+ * Call in infinite loop
+ *
+ */
 void lm_process(void);
 
 #endif /*LIGHT_MUSIC_H_*/
