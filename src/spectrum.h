06b432766ae8ad27e06ebb269a56a85419237bb6
valcheb <cheb.valentin@gmail.com>
2021-05-18 10:42:06 +0300

    refactor(spectrum): add documentation

--- src/spectrum.h
+++ src/spectrum.h
@@ -6,7 +6,18 @@
 
 #define BITCRUSH_DEPTH 64
 
+/**
+ * @brief Initialize functionality of spectrum analysis
+ *
+ */
 void spectrum_init(void);
+
+/**
+ * @brief Find spectrum magnitude maximums of sound buffer in frequency ranges
+ *
+ * @param sound Pointer to sound buffer
+ * @param coeff Pointer to resulting maximum's coefficients
+ */
 void spectrum_analysis(uint16_t *sound, uint16_t *coeff);
 
 #endif /*SPECTRUM_H_*/
