967a320debcd6f681d55a0054b9c205d4af17bc4
valcheb <cheb.valentin@gmail.com>
2021-05-18 10:48:11 +0300

    refactor(player): add documentation

--- src/player.h
+++ src/player.h
@@ -3,8 +3,24 @@
 
 #include <stdint.h>
 
+/**
+ * @brief Initialize functionality of CL43L22 as player
+ *
+ */
 void player_init(void);
+
+/**
+ * @brief Enable functionality of CL43L22
+ *
+ */
 void player_enable(void);
+
+/**
+ * @brief Send sound data to CL43L22
+ *
+ * @param buffer Pointer to sound buffer
+ * @param size   Size of sound buffer
+ */
 void player_send(uint16_t *buffer, int size);
 
 #endif /*PLAYER_H_*/
